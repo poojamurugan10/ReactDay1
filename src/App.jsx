13f6ba120ef64f850e1a33147f5b5d906d766cad
poojamurugan10 <poojaudhaya24@gmail.com>
2025-04-19 23:25:33 +0530

    base code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//import part
+import React from 'react';
+
+const App = () => {
+    //js part
+    return (
+        //html part
+        <>
+            <h1>Hello World</h1>
+        </>
+    );
+};
+//export part
+export default App;
